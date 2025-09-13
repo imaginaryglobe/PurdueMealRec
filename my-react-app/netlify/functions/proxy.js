f1da1a2e4b6e2a9ee132585388e6ba497ac5a752
Jaden Fang <jadenfang9@gmail.com>
2025-09-13 09:36:10 -0400

    Add netlify.toml and fix CORS preflight in proxy function

--- my-react-app/netlify/functions/proxy.js
+++ my-react-app/netlify/functions/proxy.js
@@ -1,4 +1,17 @@
 exports.handler = async (event, context) => {
+  // Handle CORS preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      },
+      body: '',
+    };
+  }
+
   // Only allow POST requests
   if (event.httpMethod !== 'POST') {
     return {
